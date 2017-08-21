f15125eca3d533e8729245f819151d2d44970044
Charlie Andrews-Jubelt <cajubelt@mit.edu>
2017-08-21 14:24:13 -0700

    successfully navigating to climber profile, avoiding irritating workaround I resorted to yesterday with crag search

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,8 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/scrape8a', function(req,res,next){
+	var member_name = 'charlie andrews'; //TODO replace with inputs to the route
+
 	var driver = new webdriver.Builder()
 	    .forBrowser('chrome')
 	    .build();
@@ -23,45 +25,50 @@ router.get('/scrape8a', function(req,res,next){
 	// 	console.log('Page title is: ' + title);
  //  		res.render('scrape_results', {scrape: title});
 	// });
-
-	driver.get('https://www.8a.nu');
+	
+	//Navigate to route and member search page
+	driver.get('https://www.8a.nu/scorecard/search.aspx');
 	driver.switchTo().frame('main');
+
+	//Search for climber (8a member) by name
+	var climber_name = driver.findElement(webdriver.By.name('TextboxMemberName'));
+	climber_name.sendKeys(member_name);
+	climber_name.submit();
+	var search_button = driver.findElement(webdriver.By.name('ButtonSearchMember'));
+	search_button.click();
+
+	//Pull up profile of that climber
+	var profile_link = driver.findElement(webdriver.By.xpath('//*[contains(text(), "Charlie Andrews")]'));
+	profile_link.click();
+
 	// driver.switch_to.frame('main');
 
 	// var ranking = driver.findElement(webdriver.By.xpath("//*[contains(text(),'Ranking')]"));
 	// ranking.click();
 
-	//search an arbitrary crag name
-	var fill_field = driver.findElement(webdriver.By.name('CragName')).sendKeys('xxxxx');
-	fill_field.then(function(){
-		// driver.findElement(webdriver.By.name('ButtonSearch')).click();
-		setTimeout(function(){
-			driver.findElement(webdriver.By.name('ButtonSearch')).click();
-		},1000);
-	});
-	// setTimeout(function(){driver.findElement(webdriver.By.name('ButtonSearch')).click();},500);
+	//search an arbitrary crag name and click button (to get to page with member search on it)
+	// var fill_field = driver.findElement(webdriver.By.name('CragName')).sendKeys('xxxxx');
+	// fill_field.then(function(){
+	// console.log('trying to click button');
+	// setTimeout(function(){
+	// 	driver.findElement(webdriver.By.name('ButtonSearch')).click();
+	// },250);
+	// }).then(null, function(err){
+	// 	if (err.message.includes('not clickable')){//menu hiding the button still
+	// 		setTimeout(function(){ //wait and try again to click the button
+	// 			console.log('trying again to click button');
+	// 			driver.findElement(webdriver.By.name('ButtonSearch')).click();
+	// 		},2000)
+	// 	}
+	// });
+	
 
 	//report the name of the page you reach!
 	driver.getTitle().then(function(title){
 		res.render('scrape_results', {scrape: title});
+		// driver.quit();
 	});
 
-	// driver.wait(function() {
-	//   return driver.getTitle().then(function(title) {
-	//     return title.toLowerCase().lastIndexOf('cheese!', 0) === 0;
-	//   });
-	// }, 3000);
-
-	// driver.getTitle().then(function(title) {
-	//   console.log('Page title is: ' + title);
-	// });
-
-	// driver.quit();
-	// driver.get('http://www.google.com/ncr');
-	// driver.findElement(By.name('q')).sendKeys('webdriver');
-	// driver.findElement(By.name('btnG')).click();
-	// driver.wait(until.titleIs('webdriver - Google Search'), 1000);
-	// driver.quit();
 });
 
 module.exports = router;
