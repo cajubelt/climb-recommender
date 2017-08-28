2fb10000bbb2109a4b824f6468b41962847521f1
Charlie Andrews-Jubelt <cajubelt@mit.edu>
2017-08-28 16:34:19 -0700

    console logged the name of the first climb completed!

--- routes/index.js
+++ routes/index.js
@@ -33,38 +33,38 @@ router.get('/scrape8a', function(req,res,next){
 	//Search for climber (8a member) by name
 	var climber_name = driver.findElement(webdriver.By.name('TextboxMemberName'));
 	climber_name.sendKeys(member_name);
-	climber_name.submit();
+	// climber_name.submit();
 	var search_button = driver.findElement(webdriver.By.name('ButtonSearchMember'));
 	search_button.click();
 
-	//Pull up profile of that climber
+	//Pull up profile of that climber (TODO (?) store the userID of the climber to more quickly access profile in future using 8a.nu/user/profile.aspx?UserID=<user_id>)
 	var profile_link = driver.findElement(webdriver.By.xpath('//*[contains(text(), "Charlie Andrews")]'));
 	profile_link.click();
+		//You have access to 'recommended routes' from here, which may be enough.. but for now, will go for all routes completed and logged
 
-	// driver.switch_to.frame('main');
+	//Get all routes (i.e. sport climbs) logged by that climber in order of date
+	var routes_link = driver.findElement(webdriver.By.xpath('//*[contains(text(),"Routes")]'));
+	routes_link.click();
+	var all_time_link = driver.findElement(webdriver.By.xpath('//*[contains(text(),"All Time (All Ascents)")]'));
+	all_time_link.click();
+	var by_date_link = driver.findElement(webdriver.By.id('HyperLinkOrderByDate'));
+	by_date_link.click();
 
-	// var ranking = driver.findElement(webdriver.By.xpath("//*[contains(text(),'Ranking')]"));
-	// ranking.click();
-
-	//search an arbitrary crag name and click button (to get to page with member search on it)
-	// var fill_field = driver.findElement(webdriver.By.name('CragName')).sendKeys('xxxxx');
-	// fill_field.then(function(){
-	// console.log('trying to click button');
-	// setTimeout(function(){
-	// 	driver.findElement(webdriver.By.name('ButtonSearch')).click();
-	// },250);
-	// }).then(null, function(err){
-	// 	if (err.message.includes('not clickable')){//menu hiding the button still
-	// 		setTimeout(function(){ //wait and try again to click the button
-	// 			console.log('trying again to click button');
-	// 			driver.findElement(webdriver.By.name('ButtonSearch')).click();
-	// 		},2000)
-	// 	}
+	/*WORKING example of getting and logging the first climb name from a nonempty of ascents */
+	// var gb_climb_elt = driver.findElement(webdriver.By.xpath('//*[@id="form1"]//div[4]//table[3]//tbody//tr[3]//td[4]//span[2]//i//a')).getText();
+	// gb_climb_elt.then(function(name){ 
+	// 	console.log(name);
 	// });
-	
 
-	//report the name of the page you reach!
+	//NOT SURE if the following row retrieval works
+	// var first_climb_row = driver.findElement(webdriver.By.xpath('//*[@id="form1"]//div[4]//table[3]//tbody//tr[3]'));
+	// first_climb_row.then(function(row){
+	// maybe log the html of the row?
+	// })
+
+	//report the name of the page reached
 	driver.getTitle().then(function(title){
+		// console.log(climbs_list);
 		res.render('scrape_results', {scrape: title});
 		// driver.quit();
 	});
