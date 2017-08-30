c8debbea132c2544de1293c77733733a1715662c
Charlie Andrews-Jubelt <cajubelt@mit.edu>
2017-08-29 17:21:06 -0700

    implemented add climber. need to test it still.

--- models/Climbers.js
+++ models/Climbers.js
@@ -3,11 +3,12 @@ var mongoose = require('mongoose')
 // var _ = require('underscore');
 
 var climberSchema = Schema({
-    name: String,
-    
-    //extra profile info:
+    name: String, //{type: String, required: true}
+    is_female: Boolean,
     city: String,
     country: String,
+    
+    //optional profile info:
     birthday: String,
     height: String,
     weight: String,
@@ -17,7 +18,6 @@ var climberSchema = Schema({
     best_comp_result: String,
     best_climbing_area: String,
     sponsor_links: String,
-    is_female: Boolean,
 });
 
 var climberModel = mongoose.model('Climber', climberSchema);
@@ -30,6 +30,52 @@ var errorUtils = require('../utils/errorUtils');
 var Climbers = (function(climberModel) {
 
     var that = {};
+
+    // that.addAdmin = function(emailAddress, password, callback) {
+    //     var inputError = checkForValidInputs(emailAddress, password);
+    //     if (inputError) {
+    //         callback({msg: inputError});
+    //     } else {
+    //         adminModel.findOne({emailAddress: emailAddress}, function(err, admin) {
+    //             if (admin) {
+    //                 callback({msg: errorUtils.DuplicateAccount()});
+    //             } else {
+    //                 var admin = new adminModel({
+    //                     emailAddress: emailAddress,
+    //                     password: password
+    //                 });
+    //                 admin.save(function(err, newAdmin) {
+    //                     if (err) callback({msg: err});
+    //                     else callback(null, newAdmin);
+    //                 })
+    //             }
+    //         });
+    //     }
+    // };
+
+    that.addClimber = function(name, is_female, city, country, optional_info, callback){
+        if (!name || !city || !country || !optional_info || !callback){
+            callback({msg: errorUtils.InvalidInputs()});
+        } else {
+            climberModel.findOne({name:name, is_female: is_female, city: city, country: country}, function(err, climber){
+                if (climber){
+                    //TODO check additional details about the climber against the potential duplicate
+                    callback({msg: errorUtils.DuplicateClimber()});
+                } else {
+                    var climber_info = JSON.parse(JSON.stringify(optional_info));
+                    climber_info.name = name;
+                    climber_info.is_female = is_female;
+                    climber_info.city = city;
+                    climber_info.country = country;
+                    var climber = new climberModel(climber_info);
+                    climber.save(function(err, newClimber){
+                        if (err) callback({msg: err});
+                        else callback(null, newClimber);
+                    });
+                }
+            });
+        }
+    }
 
     Object.freeze(that);
     return that;
--- utils/errorUtils.js
+++ utils/errorUtils.js
@@ -2,10 +2,15 @@ var error_utils = (function() {
 
     var that = {};
 
-    //already exists (creating a new account)
-    // that.DuplicateAccount = function() {
-    //     return "An account already exists for this email address.";
-    // }
+    //already exists (creating a new Climber in the database)
+    that.DuplicateClimber = function() {
+        return "A climber already exists with the information provided.";
+    }
+
+    //invalid inputs
+    that.InvalidInputs = function(){
+        return "Invalid inputs provided for the database query."
+    }
 
     Object.freeze(that);
     return that;
